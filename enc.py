de9bd6816bdffe2a3af8a4e7e37e7ab1638dfebc
Stanley Davies <stan07davies@gmail.com>
2026-08-21 13:34:12 +0100

    Rewrote encoding to use a dictionary based system.

--- enc.py
+++ enc.py
@@ -1,14 +1,16 @@
+# Currently only works on lowercase text.
+
+import re
+import util
+
 with open("plaintext", "r") as f:
         plain  = f.read()
 
 plain += '\0'
 
+symbols = "".join(filter(lambda x: (x >= 'a' and x <= 'z') or ' ' == x, plain)).split(' ')
+
 reps = {}
-sym = ""
-
-symbols = "".join(filter(lambda x: (x >= 'a' and x <= 'z') or ' ' == x, plain.lower())).split(' ')
-
-# Silly one liner for this?
 for s in symbols:
         if s in reps:
                 reps[s] += 1
@@ -18,4 +20,9 @@ for s in symbols:
 # Might want to work out optimum rather than just picking a value.
 filtered = [key for key in reps if reps[key] > 1]
 
-print(f"{filtered = }")
+
+with open("enc.bin", "wb") as f:
+        for i in range(0, len(filtered)):
+                plain = re.sub(f'{filtered[i]}', f'{chr(30)}{i}', plain)
+                f.writelines([ord(c).to_bytes(1) for c in f'{i}{filtered[i]}\n'])
+        f.writelines([ord(c).to_bytes(1) for c in plain])
