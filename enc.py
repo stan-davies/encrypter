0608876b4b889aed266fac02857a14e995a0518f
Stanley Davies <stan07davies@gmail.com>
2026-08-21 10:39:26 +0100

    Started doing the dictionary thing and remembered python is a barbaric language.

--- enc.py
+++ enc.py
@@ -1,36 +1,21 @@
-import util
-
-
-# So the encoded text will just have to be not really unicode. Probably have
-# to write to the file as binary. Letters and numbers and so on will have the
-# same identifiers, but then other stuff is being changed, so 30 will
-# represent start of repitition block, and it expressly doesn't mean what it
-# normally does.
-
-
 with open("plaintext", "r") as f:
         plain  = f.read()
 
-pln_bt = [ord(c).to_bytes(1) for c in plain]
-enc_bt = []
+plain += '\0'
 
-reps = 1 # every letter has one repitition of itself
-prec = (0).to_bytes(1)
-for c in pln_bt:
-        if prec == c:
-                reps += 1
-        elif reps >= 4:         # Minimum for actually saving bytes in enc.
-                del enc_bt[len(enc_bt) - reps:]
-# Convert to '0xXX', minus 1 to give number of columns plus one, then divide,
-# so 1,2 columns goes to 1 byte, 2,3 columns to 2B, 4,5 columns to 3B, ...
-                width = (len(f"{hex(reps)}") - 1) // 2
-                enc_bt.extend([util.ESCb, reps.to_bytes(width), util.ESCb, prec])
-                reps = 1
+reps = {}
+sym = ""
+
+symbols = "".join(filter(lambda x: (x >= 'a' and x <= 'z') or ' ' == x, plain.lower())).split(' ')
+
+# Silly one liner for this?
+for s in symbols:
+        if s in reps:
+                reps[s] += 1
         else:
-                reps = 1
+                reps[s] = 1
 
-        enc_bt.append(c)
-        prec = c
+# Might want to work out optimum rather than just picking a value.
+filtered = [key for key in reps if reps[key] > 1]
 
-with open("enc.bin", "wb") as f:
-        f.writelines(enc_bt)
+print(f"{filtered = }")
